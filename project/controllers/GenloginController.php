c531692546e1f5bf7ab811cb9c83b1d93afa2207
Mojusei <metra9991@mail.ru>
2025-02-12 18:25:30 +0400

    Предрелизная стабильная версия
    Добавлена логика генератора, немного доработан дизайн страницы.
    Первые тесты прошли успешно

--- project/controllers/GenloginController.php
+++ project/controllers/GenloginController.php
@@ -5,9 +5,105 @@
 	
 	class GenloginController extends Controller
 	{
-		public function show() {
-			$this->title = 'Генератор логинов';
+		private string $login;
+		private array $chars;
+		private int $loginLength;
 
-            return $this->render('genlogin/show');
+		public function __construct()
+		{
+			$this->login = '';
+			$this->chars = array_merge(range('a', 'z'), range('A', 'Z'));
+			$this->loginLength = $this->validateLoginLength();
 		}
+
+
+		public function show()
+		{
+			// Устанавливает заголовок, рендерит блок с формой и передает туда готовый логин
+
+			$this->title = 'Генератор логинов';
+			
+			if (!empty($_POST))
+			{
+				$this->login = $this->createLogin($this->createCharsArray());
+			}
+
+            return $this->render('genlogin/show', [
+				'login' => $this->login
+			]);
+		}
+
+
+		private function createLogin($data) : string
+		{
+			// Делает строку из готового массива
+			shuffle($data);
+        	$login = implode($data);
+
+        	return $login;
+		}
+
+		private function createCharsArray() : array
+		{
+			// Создает массив букв в случайном порядке
+			// Если количество полученных букв меньше чем длина логина, то перемашивает полученные буквы с буквами из chars
+			// В противном случае использует только массив полученных из формы букв
+
+			$selectedChars = $this->validateSelectedChars();
+			
+			$countSelectedChars = count($selectedChars);
+
+			if ($countSelectedChars < $this->loginLength and $countSelectedChars > 0)
+			{
+				$selectedCharsPart = $this->randomCharsArray($selectedChars, $countSelectedChars);
+				$restCharsPart = $this->randomCharsArray($this->chars, ($this->loginLength - $countSelectedChars));
+
+				return array_merge($selectedCharsPart, $restCharsPart);
+			}
+			else
+			{
+				$restCharsPart = $this->randomCharsArray($this->chars, ($this->loginLength - $countSelectedChars));
+
+				return $restCharsPart;
+			}
+		}
+
+		private function randomCharsArray($data, $length) : array
+		{
+			return array_rand(array_flip($data), $length);
+		}
+
+		private function validateSelectedChars() : array
+		{
+			// Проверяет буквы из формы, создает из них массив или возвращает пустой
+
+			if (isset($_POST['chars']))
+			{
+				$pattern = '#[a-zA-Z]#';
+				preg_match_all($pattern, $_POST['chars'], $matches);
+				
+				return $matches[0];
+			}
+			else 
+			{
+				return [];
+			}
+		}
+
+
+		private function validateLoginLength() : int
+		{
+			// Проверяет, что получено число и возвращает его, в противном случае устанавливает значение минимальной длины логина
+
+			if(!empty($_POST['length']) and is_numeric($_POST['length']))
+			{
+				return ((int) $_POST['length']);
+			}
+			else
+			{
+				return 6;
+			}
+		}
+
+		
 	}
--- project/views/genlogin/show.php
+++ project/views/genlogin/show.php
@@ -1,23 +1,24 @@
 <div class="container">
     <div class="row align-items-center g-lg-5 py-5">
       <div class="col-lg-7 text-center text-lg-start">
-        <h1 class="display-4 fw-bold lh-1 text-body-emphasis mb-3">Генератор логинов</h1>
-        <p class="col-lg-10 fs-4"></p>
+        <h1 class="text-center display-4 fw-bold lh-1 text-body-emphasis mb-5">Генератор логинов</h1>
+        <h2 class="text-center pt-5"><?= $login ?></h2>
       </div>
+
       <div class="col-md-10 mx-auto col-lg-5">
-        <form class="p-4 p-md-5 border rounded-3 bg-body-tertiary" >
-          <div class="form-floating mb-3">
+        <form class="p-4 p-md-5 border rounded-3 bg-body-tertiary" action="" method="POST">
+          <div class="form-floating mb-5">
             <p class="col-lg-10 fs-4">Длина логина: <span id="rangeValue">9</span> символов</p>
-            <input type="range" class="form-range" min="6" max="12" id="slider" onchange="document.getElementById('rangeValue').innerHTML = this.value;">
-            
+            <input name="length" type="range" class="form-range" min="6" max="12" id="slider" onchange="document.getElementById('rangeValue').innerHTML = this.value;"> 
           </div>
-          <div class="form-floating mb-3">
-            <input type="text" class="form-control" id="floatingPassword" placeholder="Password">
-            <label for="floatingPassword">Password (пока что)</label>
+
+          <div class="form-floating mb-3 ">
+            <input name="chars" type="text" class="form-control" id="selectedChars" placeholder="abcd">
+            <label for="selectedChars">Выбранные буквы в формате 'abcd'</label>
           </div>
           <button class="w-100 btn btn-lg btn-primary" type="submit">Генерация</button>
           <hr class="my-4">
-          <small class="text-body-secondary">Логин появится снизу заголовка</small>
+          <small class="text-body-secondary">Оставьте поле пустым, если не выбрали конкретных букв</small>
         </form>
       </div>
     </div>
